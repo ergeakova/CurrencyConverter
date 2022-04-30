634c0a53044c1895615cc662c7869b98644d8302
ergeakova <ergeak@gmail.com>
2022-04-30 23:56:48 +0300

    UI desing

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var lblTry: UILabel!
+    @IBOutlet weak var lblUsd: UILabel!
+    @IBOutlet weak var lblJpy: UILabel!
+    @IBOutlet weak var lblGbp: UILabel!
+    @IBOutlet weak var lblChf: UILabel!
+    @IBOutlet weak var lblCad: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func getRates(_ sender: Any) {
+        print("get rates")
+    }
+    
 }
 
